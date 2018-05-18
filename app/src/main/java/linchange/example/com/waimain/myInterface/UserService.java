1466cb85e00577792aa1342664e920d9957c840b
styz <634909510@qq.com>
2018-05-18 22:26:49 +0800

    个性化设置

--- app/src/main/java/linchange/example/com/waimain/myInterface/UserService.java
+++ app/src/main/java/linchange/example/com/waimain/myInterface/UserService.java
@@ -3,8 +3,12 @@ package linchange.example.com.waimain.myInterface;
 import java.util.List;
 
 import linchange.example.com.waimain.bean.User;
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -13,12 +17,38 @@ import retrofit2.http.Query;
  */
 
 public interface UserService {
+
+    //得到所用用户
     @GET("getUsers")
     Call<List<User>> getUsers();
 
+    //登录验证
     @GET("login/{username}/{password}")
     Call<User> login(@Path("username")String username,@Path("password")String password);
 
+    //注册用户
     @GET("newUser")
     Call<Boolean> register(@Query("username") String username, @Query("password") String password);
+
+    //更新昵称
+    @GET("updateNickName/{userId}/{nickName}")
+    Call<Boolean> updateNickName(@Path("userId")String userId,@Path("nickName")String nickName);
+
+    //更新手机号
+    @GET("updatePhone/{userId}/{phone}")
+    Call<Boolean> updatePhone(@Path("userId")String userId,@Path("phone")String phone);
+
+    //更新邮箱
+    @GET("updateEmail/{userId}/{email}")
+    Call<Boolean> updateEmail(@Path("userId")String userId,@Path("email")String email);
+
+    //更新密码
+    @GET("updatePassword/{userId}/{oldPassword}/{newPassword}")
+    Call<Boolean> updatePassword(@Path("userId")String userId,@Path("oldPassword")String oldPassword,@Path("newPassword")String newPassword);
+
+    //上传头像
+    @Multipart
+    @POST("")
+    Call<Boolean> uploadImage(@Part MultipartBody.Part file);
+
 }
